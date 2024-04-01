f50f000245e88da4125455ae52c9869651c5bcdf
elif <147872048+elifdev@users.noreply.github.com>
2024-04-01 23:21:38 +0300

    Delete SoftwareLanguage directory

--- SoftwareLanguage/src/main/java/com/software/repository/LanguageTechRepository.java
+++
@@ -1,9 +0,0 @@
-package com.software.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.software.entities.LanguageTech;
-
-public interface LanguageTechRepository
-		extends JpaRepository<LanguageTech, Integer> {
-}
